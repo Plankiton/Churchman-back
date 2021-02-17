6074745c1c6633516858c5d9eaa91f31f2a7c298
Plankiton <pl4nk1ton@gmail.com>
2021-02-17 14:01:20 -0300

    [Miscellanous] Inprove API test

--- tests/api.go
+++ tests/api.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"net/http"
-	"os"
+    "encoding/json"
+    "net/http"
+    "bytes"
+    "os"
 
-	"../api"
-	"github.com/Coff3e/Api"
+    "../api"
+    "github.com/Coff3e/Api"
 )
 
 func main() {
@@ -20,5 +22,55 @@ func main() {
             Data: "JOAO É GAYZÃOOO!!",
         }, 200
     })
-    r.Run("/", 8000)
+    go r.Run("/", 8000)
+
+    for _, url := range []string{"http://localhost:8000/", "http://localhost:8000/joao"} {
+        body := new(bytes.Buffer)
+        json.NewEncoder(body).Encode(api.Request {
+            Token: "1290839028903809",
+            Data: map[string]interface{}{
+                "Joao": "Maria",
+                "Marta": 90,
+                "Cellbit": false,
+            },
+        })
+        api.Log("Trying to make requests on", url)
+
+        res, err := http.Get(url)
+        if (err == nil) {
+            r_body := api.Response{}
+            json.NewDecoder(res.Body).Decode(&r_body)
+            res.Body.Close()
+
+            r_raw_body := new(bytes.Buffer)
+            r_raw_body.ReadFrom(res.Body)
+
+            if  r_body.Type == "Error" {
+                api.War("Sucessfull request with error\n\t-> Response:", r_raw_body)
+            } else {
+                api.Log("Sucessfull request\n\t-> Response:", r_raw_body)
+            }
+        } else {
+            api.Err("Failure request sending, ", err)
+        }
+
+        res, err = http.Post(url, "application/json", body)
+        if (err == nil){
+            r_body := api.Response{}
+            json.NewDecoder(res.Body).Decode(&r_body)
+            res.Body.Close()
+
+            r_raw_body := new(bytes.Buffer)
+            r_raw_body.ReadFrom(res.Body)
+
+            if  r_body.Type == "Error" {
+                api.War("Sucessfull request with error\n\t-> Response:", r_raw_body)
+            } else {
+                api.Log("Sucessfull request\n\t-> Response:", r_raw_body)
+            }
+        } else {
+            api.Err("Failure request sending, ", err)
+        }
+
+    }
 }
