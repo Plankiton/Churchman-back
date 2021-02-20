81863d75fabdbdc3d1d1a8b915225b53066cb90d
Plankiton <pl4nk1ton@gmail.com>
2021-02-20 00:06:31 -0300

    [Feature] Adiciona rota /login para o main do projeto

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
     "os"
 
-    "github.com/Coff3e/Church-app/api"
+    "./api"
     "github.com/Coff3e/Api"
 )
 
 func main() {
     con_str := "host=localhost user=plankiton password=joaojoao dbname=church port=5432 sslmode=disable TimeZone=America/Araguaina"
-    _, err := church.SignDB(con_str, api.Postgres)
+    r := church.Church{}
+    _, err := r.SignDB(con_str, api.Postgres)
     if (err != nil) {
         os.Exit(1)
     }
     api.Log("Database connected with sucess")
+    r.Add(
+        "post", "/login", api.RouteConf {}, church.Login,
+    )
+    r.Run("/", 8000)
 }
